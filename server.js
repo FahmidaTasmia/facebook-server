f7e014970c737e5531442cd782058f9993a855f1
FahmidaTasmia <tasmia1070@gmail.com>
2022-12-30 19:35:56 +0600

    mongodb and mongose connected

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-
+mongoose.set('strictQuery', false);
 const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
+app.use(express.json());
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.listen(8000, () => {
-  console.log("server is lestining...");
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}..`);
 });
